c9264d9dd59ace510917c6e7d1b959260f22fa51
Korsen <f.korsen@googlemail.com>
2015-08-02 11:04:10 +0200

    Initial commit

---
+++ app/src/main/java/com/korsen/seifenblasen/Seifenblasen.java
@@ -0,0 +1,18 @@
+package com.korsen.seifenblasen;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Seifenblasen extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seifenblasen);
+    }
+}
